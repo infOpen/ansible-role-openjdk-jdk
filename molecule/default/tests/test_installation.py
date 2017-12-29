f7bc63a167a6e429529edbf54ed4a5d219a14207
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2017-12-29 17:55:12 +0100

    Bump molecule 1.25.0 -> 2.6.0

--- molecule/default/tests/test_installation.py
+++ molecule/default/tests/test_installation.py
@@ -2,9 +2,11 @@
 Role tests
 """
 
+import os
 from testinfra.utils.ansible_runner import AnsibleRunner
 
-testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
+testinfra_hosts = AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
 def test_packages(host):
